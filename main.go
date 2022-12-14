76c672cab45c9eb0a9e28e90b4c381df2f6898e6
Asem <student@ALEM-F4-R02-03.alem.school>
2022-12-14 19:02:53 +0600

    Routing Requets

--- main.go
+++ main.go
@@ -7,13 +7,27 @@ import (
 
 // функция-обработчик
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+func showSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific anippet..."))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet"))
 }
 
 func main() {
 	// роутер
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet", showSnippet)
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Starting server on :4000\nhttp://localhost:4000/")
 	// запускаем новый веб-сервер через функциюю http.ListenAndServe
